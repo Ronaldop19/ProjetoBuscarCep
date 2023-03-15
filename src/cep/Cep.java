6491bba5025f5618b237463aa7eed3325be03d05
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2023-03-14 21:28:19 -0300

    ajustes

--- src/cep/Cep.java
+++ src/cep/Cep.java
@@ -1,28 +1,34 @@
 package cep;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Toolkit;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import java.awt.Font;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import java.awt.SystemColor;
 import java.awt.Cursor;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import Atxy2k.CustomTextField.RestrictedTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
 public class Cep extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtCep;
+	private JTextField txtEndereco;
+	private JTextField txtBairro;
+	private JTextField txtCidade;
 
 	/**
 	 * Launch the application.
@@ -54,79 +60,104 @@ public class Cep extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("CEP");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 15));
 		lblNewLabel.setBounds(24, 25, 58, 24);
 		contentPane.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		textField.setBounds(98, 29, 116, 19);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
+
+		txtCep = new JTextField();
+		txtCep.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtCep.setBounds(98, 29, 116, 19);
+		contentPane.add(txtCep);
+		txtCep.setColumns(10);
+
 		JLabel lblNewLabel_1 = new JLabel("Endereço");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_1.setBounds(24, 72, 80, 18);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Bairro");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_2.setBounds(24, 132, 80, 13);
 		contentPane.add(lblNewLabel_2);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Cidade");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_3.setBounds(24, 193, 48, 13);
 		contentPane.add(lblNewLabel_3);
-		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		textField_1.setBounds(98, 74, 270, 19);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		textField_2.setBounds(98, 129, 270, 19);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		textField_3.setBounds(98, 190, 187, 19);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
-		
+
+		txtEndereco = new JTextField();
+		txtEndereco.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtEndereco.setBounds(98, 74, 270, 19);
+		contentPane.add(txtEndereco);
+		txtEndereco.setColumns(10);
+
+		txtBairro = new JTextField();
+		txtBairro.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtBairro.setBounds(98, 129, 270, 19);
+		contentPane.add(txtBairro);
+		txtBairro.setColumns(10);
+
+		txtCidade = new JTextField();
+		txtCidade.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtCidade.setBounds(98, 190, 187, 19);
+		contentPane.add(txtCidade);
+		txtCidade.setColumns(10);
+
 		JLabel lblNewLabel_4 = new JLabel("UF");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_4.setBounds(295, 193, 24, 13);
 		contentPane.add(lblNewLabel_4);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		comboBox.setBounds(329, 191, 80, 21);
-		contentPane.add(comboBox);
-		
-		JButton btnNewButton = new JButton("Limpar");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnNewButton.setBounds(36, 232, 85, 21);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Buscar");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnNewButton_1.setBounds(239, 28, 85, 21);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("");
-		btnNewButton_2.setToolTipText("Sobre");
-		btnNewButton_2.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
-		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_2.setBorder(null);
-		btnNewButton_2.setBackground(SystemColor.control);
-		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnNewButton_2.setBounds(341, 10, 48, 48);
-		contentPane.add(btnNewButton_2);
-	}
+
+		JComboBox cboUf = new JComboBox();
+		cboUf.setModel(new DefaultComboBoxModel(
+				new String[] { "", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
+						"PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO" }));
+		cboUf.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		cboUf.setBounds(329, 191, 80, 21);
+		contentPane.add(cboUf);
+
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnLimpar.setBounds(36, 232, 85, 21);
+		contentPane.add(btnLimpar);
+
+		JButton btnCep = new JButton("Buscar");
+		btnCep.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (txtCep.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Informe o CEP");
+					txtCep.requestFocus();
+				} else {
+					// buscar CEP
+					
+				}
+			}
+		});
+		btnCep.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnCep.setBounds(239, 28, 85, 21);
+		contentPane.add(btnCep);
+
+		JButton btnSobre = new JButton("");
+		btnSobre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Sobre sobre = new Sobre();
+				sobre.setVisible(true);
+			}
+		});
+		btnSobre.setToolTipText("Sobre");
+		btnSobre.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
+		btnSobre.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnSobre.setBorder(null);
+		btnSobre.setBackground(SystemColor.control);
+		btnSobre.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnSobre.setBounds(341, 10, 48, 48);
+		contentPane.add(btnSobre);
+
+		/* Uso da biblioteca Atxy2k para a validação do campo txt Cep */
+		RestrictedTextField validar = new RestrictedTextField(txtCep);
+		validar.setOnlyNums(true);
+		validar.setLimit(8);
+	}// end constructor
 }
--- src/cep/Sobre.java
+++ src/cep/Sobre.java
@@ -1,14 +1,20 @@
 package cep;
 
-import java.awt.EventQueue;
-
-import javax.swing.JDialog;
-import java.awt.Toolkit;
-import javax.swing.JLabel;
-import java.awt.SystemColor;
-import javax.swing.JButton;
 import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.EventQueue;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.net.URI;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Sobre extends JDialog {
 
@@ -33,10 +39,11 @@ public class Sobre extends JDialog {
 	 * Create the dialog.
 	 */
 	public Sobre() {
+		setModal(true);
 		setResizable(false);
 		setTitle("Sobre");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/img/home.png")));
-		setBounds(100, 100, 450, 300);
+		setBounds(150, 150, 450, 300);
 		getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Buscar CEP - Ver 1.0");
@@ -51,29 +58,55 @@ public class Sobre extends JDialog {
 		lblNewLabel_2.setBounds(30, 122, 82, 13);
 		getContentPane().add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("republicavirtual.com.br");
-		lblNewLabel_3.setForeground(SystemColor.textHighlight);
-		lblNewLabel_3.setBounds(121, 122, 206, 13);
-		getContentPane().add(lblNewLabel_3);
+		JLabel lblWebService = new JLabel("republicavirtual.com.br");
+		lblWebService.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				link("https://republicavirtual.com.br/");
+			}
+		});
+		lblWebService.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblWebService.setForeground(SystemColor.textHighlight);
+		lblWebService.setBounds(121, 122, 206, 13);
+		getContentPane().add(lblWebService);
 		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.setToolTipText("LinkedIn");
-		btnNewButton.setIcon(new ImageIcon(Sobre.class.getResource("/img/linkedin.png")));
-		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton.setBorder(null);
-		btnNewButton.setBackground(SystemColor.control);
-		btnNewButton.setBounds(43, 193, 69, 48);
-		getContentPane().add(btnNewButton);
+		JButton btnLinkedin = new JButton("");
+		btnLinkedin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				link("https://linkedin.com/in/ronaldo-amorim-861655234/");
+			}
+		});
+		btnLinkedin.setToolTipText("LinkedIn");
+		btnLinkedin.setIcon(new ImageIcon(Sobre.class.getResource("/img/linkedin.png")));
+		btnLinkedin.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnLinkedin.setBorder(null);
+		btnLinkedin.setBackground(SystemColor.control);
+		btnLinkedin.setBounds(43, 193, 69, 48);
+		getContentPane().add(btnLinkedin);
 		
-		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.setToolTipText("GitHub");
-		btnNewButton_1.setIcon(new ImageIcon(Sobre.class.getResource("/img/github.png")));
-		btnNewButton_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_1.setBorder(null);
-		btnNewButton_1.setBackground(SystemColor.control);
-		btnNewButton_1.setBounds(219, 193, 58, 48);
-		getContentPane().add(btnNewButton_1);
+		JButton btnGithub = new JButton("");
+		btnGithub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				link("https://github.com/Ronaldop19");
+			}
+		});
+		btnGithub.setToolTipText("GitHub");
+		btnGithub.setIcon(new ImageIcon(Sobre.class.getResource("/img/github.png")));
+		btnGithub.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnGithub.setBorder(null);
+		btnGithub.setBackground(SystemColor.control);
+		btnGithub.setBounds(219, 193, 58, 48);
+		getContentPane().add(btnGithub);
 
+	}//end constructor
+
+	private void link(String site) {
+		Desktop desktop = Desktop.getDesktop();
+		try {
+			URI uri = new URI(site);
+			desktop.browse(uri);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-
 }
